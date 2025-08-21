057b6d723561ad78d1aee26787f350b9f2e779f0
Agustin Adducci <recofskydev@gmail.com>
2025-08-20 23:43:53 -0300

    [ADDON][FIX] Resource not load

--- addons/tween_orchestrator/tween_orchestrator_manager.gd
+++ addons/tween_orchestrator/tween_orchestrator_manager.gd
@@ -3,6 +3,9 @@ extends EditorPlugin
 
 const AUTOLOAD_NAME := "TweenOrchestrator"
 const AUTOLOAD_PATH := "res://addons/tween_orchestrator/code/tween_orchestrator.gd"
+const ADDON_ICON := "res://addons/tween_orchestrator/icon.png"
+
+var icons := {}
 
 func _enable_plugin() -> void:
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
@@ -11,3 +14,32 @@ func _enable_plugin() -> void:
 func _disable_plugin() -> void:
 	if ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
 		remove_autoload_singleton(AUTOLOAD_NAME)
+
+func _enter_tree() -> void:
+	icons.default = load(ADDON_ICON)
+
+	add_custom_type(
+		"TweenBinding",
+		"Resource",
+		load("res://addons/tween_orchestrator/code/tween_binding.gd"),
+		icons.default
+	)
+
+	add_custom_type(
+		"TweenClip",
+		"Resource",
+		load("res://addons/tween_orchestrator/code/tween_clip.gd"),
+		icons.default
+	)
+
+	add_custom_type(
+		"TweenTrack",
+		"Resource",
+		load("res://addons/tween_orchestrator/code/tween_track.gd"),
+		icons.default
+	)
+
+func _exit_tree() -> void:
+	remove_custom_type("TweenBinding")
+	remove_custom_type("TweenClip")
+	remove_custom_type("TweenTrack")
